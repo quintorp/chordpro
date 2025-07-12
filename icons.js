8325533366d7e4d55c7a79aec8790ba718f98852
Brad Borch <bradborch@gmail.com>
2025-07-12 07:11:09 -0400

    Update icons.js

--- icons.js
+++ icons.js
@@ -1,28 +1,34 @@
 // SVG Icons for ChordPro Viewer
 export const icons = {
-  playerTrackNext: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-player-track-next">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M2 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" />
-    <path d="M13 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" />
+  playerTrackNext: `<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" fill="currentColor">
+    <g transform="matrix(1.41176,0,0,1.49974,8.47059,-5.99802)">
+        <path d="M9,4L7,4C5.903,4 5,4.903 5,6L5,18C5,19.097 5.903,20 7,20L9,20C10.097,20 11,19.097 11,18L11,6C11,4.903 10.097,4 9,4Z" style="fill-rule:nonzero;"/>
+    </g>
+    <g transform="matrix(1.41176,0,0,1.49974,-18.3529,-5.99802)">
+        <path d="M13,5L13,19C13,19.86 14.012,20.318 14.659,19.753L22.659,12.753C22.876,12.563 23.001,12.288 23.001,12C23.001,11.712 22.876,11.437 22.659,11.247L14.659,4.247C14.012,3.682 13,4.141 13,5Z" style="fill-rule:nonzero;"/>
+    </g>
   </svg>`,
   
-  chevronDown: `<svg width="32" height="32" fill="currentColor">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z" transform="scale(1.5)" />
+  nextIcon: `<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;transform:rotate(90deg);" fill="currentColor">
+    <g transform="matrix(1.41176,0,0,1.49974,8.47059,-5.99802)">
+        <path d="M9,4L7,4C5.903,4 5,4.903 5,6L5,18C5,19.097 5.903,20 7,20L9,20C10.097,20 11,19.097 11,18L11,6C11,4.903 10.097,4 9,4Z" style="fill-rule:nonzero;"/>
+    </g>
+    <g transform="matrix(1.41176,0,0,1.49974,-18.3529,-5.99802)">
+        <path d="M13,5L13,19C13,19.86 14.012,20.318 14.659,19.753L22.659,12.753C22.876,12.563 23.001,12.288 23.001,12C23.001,11.712 22.876,11.437 22.659,11.247L14.659,4.247C14.012,3.682 13,4.141 13,5Z" style="fill-rule:nonzero;"/>
+    </g>
   </svg>`,
   
-  playerPlay: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-player-play">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
+  playerPlay: `<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;transform:rotate(90deg);" fill="currentColor">
+    <path d="M9.881,1.501L9.881,7.379L2.342,0.371C1.429,-0.476 -0,0.212 -0,1.501L0,22.497C0,23.787 1.429,24.474 2.342,23.626L9.881,16.619L9.881,22.497C9.881,23.787 11.31,24.474 12.223,23.626L23.517,13.128C23.824,12.844 24,12.431 24,11.999C24,11.566 23.824,11.154 23.517,10.87L12.223,0.371C11.31,-0.476 9.881,0.212 9.881,1.501Z" style="fill-rule:nonzero;"/>
   </svg>`,
   
-  playerPause: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause">
+  playerPause: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" />
     <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" />
   </svg>`,
   
-  lineHeight: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-line-height">
+  lineHeight: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 22 22" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-line-height">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <path d="M3 8l3 -3l3 3" />
     <path d="M3 16l3 3l3 -3" />
@@ -37,9 +43,13 @@ export const icons = {
     <path d="M17 3.34a10 10 0 1 1 -15 8.66l.005 -.324a10 10 0 0 1 14.995 -8.336m-9 1.732a8 8 0 0 0 4.001 14.928l-.001 -16a8 8 0 0 0 -4 1.072" />
   </svg>`,
   
-  fileImport: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-import">
+  fileImport: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-import">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <path d="M14 3v4a1 1 0 0 0 1 1h4" />
     <path d="M5 13v-8a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5.5m-9.5 -2h7m-3 -3l3 3l-3 3" />
+  </svg>`,
+  
+  autoPlay: `<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" fill="currentColor">
+    <path d="M9.881,1.501L9.881,7.379L2.342,0.371C1.429,-0.476 -0,0.212 -0,1.501L0,22.497C0,23.787 1.429,24.474 2.342,23.626L9.881,16.619L9.881,22.497C9.881,23.787 11.31,24.474 12.223,23.626L23.517,13.128C23.824,12.844 24,12.431 24,11.999C24,11.566 23.824,11.154 23.517,10.87L12.223,0.371C11.31,-0.476 9.881,0.212 9.881,1.501Z" style="fill-rule:nonzero;"/>
   </svg>`
 };
